4379efec333c140b9bec7303b16279ab3acb0e23
nila <thavabalanthirunilavan@gmail.com>
2020-06-09 10:08:08 +0200

    Initial commit

---
+++ pages/RegisterUser.js
@@ -0,0 +1,165 @@
+import React from 'react';
+import {View, ScrollView, KeyboardAvoidingView, Alert} from 'react-native';
+import Mytextinput from './components/Mytextinput';
+import Mybutton from './components/Mybutton';
+import {openDatabase} from 'react-native-sqlite-storage';
+var db = openDatabase({name: 'symbosis.db'});
+export default class RegisterUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      light: '',
+      soil: '',
+      watering: '',
+      image: '',
+      maximum_production: '',
+      symbioses: '',
+    };
+  }
+  register_plant = () => {
+    var that = this;
+    const {name} = this.state;
+    const {description} = this.state;
+    const {light} = this.state;
+    const {soil} = this.state;
+    const {watering} = this.state;
+    const {image} = this.state;
+    const {maximum_production} = this.state;
+    const {symbioses} = this.state;
+
+    //alert(user_name, user_contact, user_address);
+    if (name) {
+      if (description) {
+        if (light) {
+          if (soil) {
+            db.transaction(function(tx) {
+              tx.executeSql(
+                'INSERT INTO plants (name,description,light,soil,watering,image,maximum_production,symbioses) VALUES (?,?,?,?,?,?,?,?)',
+                [
+                  name,
+                  description,
+                  light,
+                  soil,
+                  watering,
+                  image,
+                  maximum_production,
+                  symbioses,
+                ],
+                // eslint-disable-next-line no-shadow
+                (tx, results) => {
+                  console.log('Results', results.rowsAffected);
+                  if (results.rowsAffected > 0) {
+                    Alert.alert(
+                      'Success',
+                      'You are Registered Successfully',
+                      [
+                        {
+                          text: 'Ok',
+                          onPress: () =>
+                            that.props.navigation.navigate('HomeScreen'),
+                        },
+                      ],
+                      {cancelable: false},
+                    );
+                  } else {
+                    // eslint-disable-next-line no-alert
+                    alert('Registration Failed');
+                  }
+                },
+              );
+            });
+          } else {
+            // eslint-disable-next-line no-alert
+            alert('Please fill Soil Level');
+          }
+        } else {
+          alert('Please fill light');
+        }
+      } else {
+        alert('Please fill description');
+      }
+    } else {
+      alert('Please fill Name');
+    }
+  };
+  render() {
+    return (
+      <View style={{backgroundColor: 'white', flex: 1}}>
+        <ScrollView keyboardShouldPersistTaps="handled">
+          <KeyboardAvoidingView
+            behavior="padding"
+            style={{flex: 1, justifyContent: 'space-between'}}>
+            <Mytextinput
+              placeholder="Enter Name"
+              onChangeText={name => this.setState({name})}
+              style={{padding: 10}}
+            />
+            <Mytextinput
+              placeholder="Description"
+              onChangeText={description => this.setState({description})}
+              maxLength={10}
+              keyboardType="numeric"
+              style={{padding: 10}}
+            />
+            <Mytextinput
+              placeholder="Light Level"
+              onChangeText={light => this.setState({light})}
+              maxLength={225}
+              numberOfLines={5}
+              multiline={true}
+              style={{textAlignVertical: 'top', padding: 10}}
+            />
+            <Mytextinput
+              placeholder="Soil Level"
+              onChangeText={soil => this.setState({soil})}
+              maxLength={225}
+              numberOfLines={5}
+              multiline={true}
+              style={{textAlignVertical: 'top', padding: 10}}
+            />
+            <Mytextinput
+              placeholder="Watering Level"
+              onChangeText={watering => this.setState({watering})}
+              maxLength={225}
+              numberOfLines={5}
+              multiline={true}
+              style={{textAlignVertical: 'top', padding: 10}}
+            />
+            <Mytextinput
+              placeholder="Image"
+              onChangeText={image => this.setState({image})}
+              maxLength={225}
+              numberOfLines={5}
+              multiline={true}
+              style={{textAlignVertical: 'top', padding: 10}}
+            />
+            <Mytextinput
+              placeholder="maximum_production"
+              onChangeText={maximum_production =>
+                this.setState({maximum_production})
+              }
+              maxLength={225}
+              numberOfLines={5}
+              multiline={true}
+              style={{textAlignVertical: 'top', padding: 10}}
+            />
+            <Mytextinput
+              placeholder="symbioses"
+              onChangeText={symbioses => this.setState({symbioses})}
+              maxLength={225}
+              numberOfLines={5}
+              multiline={true}
+              style={{textAlignVertical: 'top', padding: 10}}
+            />
+            <Mybutton
+              title="Submit"
+              customClick={this.register_plant.bind(this)}
+            />
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </View>
+    );
+  }
+}
